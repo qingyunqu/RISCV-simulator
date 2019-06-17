c0c49b8e7b5b7bce061739dec0768d79bb8dc85f
qingyunqu <yuanqiangliu25@gmail.com>
2019-06-17 19:20:29 +0800

    add part of Cache

--- src/Cache.cpp
+++ src/Cache.cpp
@@ -1,2 +1,26 @@
 #include "src/Cache.h"
 
+using namespace riscv;
+
+void Memory::HandleRequest(uint64_t addr, int32_t bytes, bool read, bool& hit, int32_t& time) {
+    hit = true;
+    time = latency.hit_latency + latency. bus_latency;
+    stats.access_time += time;
+    stats.access_counter++;
+}
+
+void Cache::initSet() {
+    this->set = new CacheSet[this->config.set_num];
+    for(int s = 0; s < config.set_num; s++){
+        this->set[s].line = new CacheLine[config.association];
+        for(int l = 0; l < config.association; l++){
+            set[s].line[l].tag = 0;
+            set[s].line[l].valid = false;
+            set[s].line[l].dirty = false;
+            set[s].line[l].not_access_time = 0;
+        }
+    }
+}
+void Cache::HandleRequest(uint64_t addr, int32_t bytes, bool read, bool& hit, int32_t& time) {
+
+}
--- src/Cache.h
+++ src/Cache.h
@@ -5,14 +5,73 @@
 
 namespace riscv {
 
+struct StorageStats {
+    int32_t access_counter;
+    int32_t miss_num;
+    int32_t access_time;
+    int32_t replace_num;
+};
+
+struct StorageLatency {
+    int32_t hit_latency;
+    int32_t bus_latency;
+};
+
+class Storage {
+public:
+    StorageStats stats;
+    StorageLatency latency;
+public:
+    void SetStats(StorageStats ss) { this->stats = ss; }
+    void SetLatency(StorageLatency sl) { this->latency = sl; }
+    virtual void HandleRequest(uint64_t addr, int32_t bytes, bool read, bool& hit, int32_t& time) = 0;
+};
+
+class Memory : public Storage {
+public:
+    virtual void HandleRequest(uint64_t addr, int32_t bytes, bool read, bool& hit, int32_t& time);
+};
+
 struct CacheConfig {
-    int size;
-    int associativity;
-    int set;
+    int32_t size;
+    int32_t association;
+    int32_t set_num;
     bool write_through;
     bool write_allocate;
-    int block_size;
-    
+    int32_t block_size;
+    int32_t s;
+    int32_t b;
+    int32_t t;
+};
+
+struct CacheLine {
+    uint64_t tag;
+    bool valid;
+    bool dirty;
+    int32_t not_access_time;
+};
+
+struct CacheSet {
+    CacheLine* line;
+};
+
+class Cache : public Storage {
+public:
+    CacheConfig config;
+    Storage* lower;
+    CacheSet* set;
+public:
+    ~Cache(){
+        for(int s = 0; s < config.set_num; s++){
+            delete[] this->set[s].line;
+        }
+        delete[] this->set;
+    }
+    void SetConfig(Cacheconfig cc) { config = cc; }
+    void SetLower(Storage* ls) { lower = ls; }
+    void initSet();
+
+    virtual void HandleRequest(uint64_t addr, int32_t bytes, bool read, bool& hit, int32_t& time);
 };
 
 } // namespace riscv
